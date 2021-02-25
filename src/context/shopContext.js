6e7e38d7f5e6dd82b63ccc614a3597edfcd9ba77
Lawrence <locaulker@gmail.com>
2021-02-25 02:40:22 -0500

    update setup

---
+++ src/context/shopContext.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import Client from "shopify-buy"
+
+// Initializing a client to return content in the store's primary language
+const client = Client.buildClient({
+  domain: "your-shop-name.myshopify.com",
+  storefrontAccessToken: "your-storefront-access-token"
+})
+
+class shopProvider extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default shopProvider
